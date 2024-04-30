531d122b5d197f02a5999508f4581a3b02e3680c
Олизарович Роман <olizarovich.r@phystech.edu>
2024-04-30 16:08:11 +0300

    'new'

---
+++ 7-1-measure.py
@@ -0,0 +1,40 @@
+
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+
+dac = [8, 11, 7, 1, 0, 5, 12, 6]
+leds = [2, 3, 4, 17, 27, 22, 10, 9]
+comp = 14
+troyka = 13
+
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(leds, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(comp, GPIO.IN)
+
+def desimal2binary(value):
+    return[int(elem) for elem in bin(value)[2:].zfill(8)]
+
+def adc():
+    value = 0
+    for i in range(7, -1, -1):
+        value += 2**i
+        dacc = []
+        dacc = desimal2binary(value)
+        GPIO.output(dac, dacc)
+        sleep(0.001)
+        compvalue = GPIO.input(comp)
+        if compvalue == 1:
+            value -= 2**i
+    return value
+
+try:
+    while True:
+        a = int(adc()/256*8)
+        b = [0]*(8-a)+[1]*a
+        print(a, b)
+        GPIO.output(leds, b)
+
+finally:
+    GPIO.cleanup()
